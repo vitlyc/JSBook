52a8fcf7f6c2f25ad91682bac2df44ca203397e7
coitusprolongatus <vitisviniferrra@yandex.ru>
2022-08-13 15:23:42 +0300

    add monaco editor

--- src/index.tsx
+++ src/index.tsx
@@ -1,27 +1,27 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import * as esbuild from "esbuild-wasm";
-import { useState, useEffect, useRef } from "react";
-import { unpkgPathPlugin } from "./plugins/unpkg-path-plugin";
-import { fetchPlugin } from "./plugins/fetch-plugin";
+import * as esbuild from 'esbuild-wasm';
+import { useState, useEffect, useRef } from 'react';
+import ReactDOM from 'react-dom';
+import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
+import { fetchPlugin } from './plugins/fetch-plugin';
+import CodeEditor from './components/code-editor';
 
 const App = () => {
   const ref = useRef<any>();
   const iframe = useRef<any>();
-  const [input, setInput] = useState("");
-  const [code, setCode] = useState("");
+  const [input, setInput] = useState('');
+  const [code, setCode] = useState('');
 
   const startService = async () => {
     ref.current = await esbuild.startService({
       worker: true,
-      wasmURL: "https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm",
+      wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm',
     });
   };
   useEffect(() => {
     startService();
   }, []);
 
-  const onClick = async (input: string) => {
+  const onClick = async () => {
     if (!ref.current) {
       return;
     }
@@ -29,18 +29,18 @@ const App = () => {
     iframe.current.srcdoc = html;
 
     const result = await ref.current.build({
-      entryPoints: ["index.js"],
+      entryPoints: ['index.js'],
       bundle: true,
       write: false,
       plugins: [unpkgPathPlugin(), fetchPlugin(input)],
       define: {
-        "process.env.NODE_ENV": '"production"',
-        global: "window",
+        'process.env.NODE_ENV': '"production"',
+        global: 'window',
       },
     });
 
     // setCode(result.outputFiles[0].text);
-    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, "*");
+    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*');
   };
 
   const html = `
@@ -65,23 +65,18 @@ const App = () => {
 
   return (
     <div>
+      <CodeEditor />
       <textarea
         value={input}
-        onChange={(e) => {
-          onClick(e.target.value);
-          setInput(e.target.value);
-        }}
+        onChange={(e) => setInput(e.target.value)}
       ></textarea>
-      <div>{/* <button onClick={onClick}>Submit</button> */}</div>
+      <div>
+        <button onClick={onClick}>Submit</button>
+      </div>
       <pre>{code}</pre>
       <iframe ref={iframe} sandbox="allow-scripts" srcDoc={html} />
     </div>
   );
 };
 
-const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+ReactDOM.render(<App />, document.querySelector('#root'));
