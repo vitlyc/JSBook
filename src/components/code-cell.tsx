fb9efe703f8b141bf5d6eaa6c54bdb9788025064
coitusprolongatus <vitisviniferrra@yandex.ru>
2022-08-17 19:12:18 +0300

    Resizable

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -1,11 +1,12 @@
-import { useState } from 'react';
-import CodeEditor from './code-editor';
-import Preview from './preview';
-import bundle from '../bundler';
+import { useState } from "react";
+import CodeEditor from "./code-editor";
+import Preview from "./preview";
+import bundle from "../bundler";
+import Resizable from "./resizable";
 
 const CodeCell = () => {
-  const [code, setCode] = useState('');
-  const [input, setInput] = useState('');
+  const [code, setCode] = useState("");
+  const [input, setInput] = useState("");
 
   const onClick = async () => {
     const output = await bundle(input);
@@ -13,16 +14,15 @@ const CodeCell = () => {
   };
 
   return (
-    <div>
-      <CodeEditor
-        initialValue="const a = 1;"
-        onChange={(value) => setInput(value)}
-      />
+    <Resizable direction="vertical">
       <div>
-        <button onClick={onClick}>Submit</button>
+        <CodeEditor initialValue="const a = 1;" onChange={(value) => setInput(value)} />
+        <div>
+          <button onClick={onClick}>Submit</button>
+        </div>
+        <Preview code={code} />
       </div>
-      <Preview code={code} />
-    </div>
+    </Resizable>
   );
 };
 
